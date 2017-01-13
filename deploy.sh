c4aeb128d6f1a5f41be836a866ad8ca20cf6df8d
Ed Sykes <ed.sykes@gmail.com>
2017-01-13 09:21:40 +0000

    changed to use home variable rather than tilde expansion; changed terraform to include path

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
+#!/usr/bin/env sh
+
+whoami
 echo "what key name?"
 read keyname
-ssh-keygen -t rsa -b 2048 -C "ed@1self.co" -N "" -f ~/.ssh/$keyname
-ssh-add ~/.ssh/$keyname
+echo "generating key"
+ls $HOME/.ssh
+ssh-keygen -t rsa -b 2048 -C "ed@1self.co" -N "" -f "$HOME/.ssh/$keyname"
+echo "adding key"
+ssh-add "$HOME/.ssh/$keyname"
 export deployment_id=$keyname
-tf apply -var 'key_name=$keyname' -var 'public_key_path=/Users/edwardsykes1/.ssh/$keyname.pub' -var 'deployment_id=$keyname'
+"starting terraform apply"
+/usr/local/bin/terraform apply -var 'key_name=$keyname' -var "public_key_path=/Users/edwardsykes1/.ssh/$keyname.pub" -var "deployment_id=$keyname" -var "profile=1self-terraform"
