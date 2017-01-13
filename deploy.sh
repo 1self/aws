4aded7e0c01c1cfd2bbd87bfe1b4c290824d0c17
Ed Sykes <ed.sykes@gmail.com>
2017-01-13 07:34:48 +0000

    added lines to deploy via terraform

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 echo "what key name?"
 read keyname
 ssh-keygen -t rsa -b 2048 -C "ed@1self.co" -N "" -f ~/.ssh/$keyname
-ssh-add $keyname
-export deployment_id=$keyname 
+ssh-add ~/.ssh/$keyname
+export deployment_id=$keyname
+tf apply -var 'key_name=$keyname' -var 'public_key_path=/Users/edwardsykes1/.ssh/$keyname.pub' -var 'deployment_id=$keyname'
